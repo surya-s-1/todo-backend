28324b0adf208bddff58c9429b4073d0feef9c70
Surya S <suryah2806@gmail.com>
2025-03-22 00:52:02 +0530

    Allowing all headers

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
+    allowedHeaders: '*',
     credentials: true
   })
   await app.listen(process.env.PORT ?? 3000);
